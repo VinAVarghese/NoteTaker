b6f947ec34cd676a501637e175e9e34587143afd
Vins MacBook <vinavarghese@gmail.com>
2020-07-11 12:07:35 -0700

    added db json and db js that reads and writes note data. got api notes for server js. Need to work to connect front end to server? Saving what I have so far

---
+++ Develop/db/DB.js
@@ -0,0 +1,37 @@
+const util = require("util")
+const fs = require("fs")
+const notesData = "./Develop/db/db.json"
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+
+class DB {
+    async readNotes(){
+        try {
+            const notesRaw = await readFileAsync(notesData, "utf8")
+            return notesRaw?JSON.parse(notesRaw):[]
+        } catch(err){
+            console.log("Somthing wen wrong while READING notes: " + err);
+        }
+    }
+    async writeNotes(notesArr){
+        try {
+          await writeFileAsync(notesData, JSON.stringify(notesArr))
+        } catch(err){
+            console.log("Something went wrong while WRITING notes ", err)
+        }
+    }
+}
+
+module.exports = new DB();
+
+// const testDB = new DB();
+// const test = async ()=>{
+//     await testDB.writeNotes({
+//         title:"Test Note!!!!",
+//         text: "Test text content!!!!"
+//     })
+//     console.log(await testDB.readNotes())
+// }
+// test()
--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 const express = require('express')
+const path = require('path')
+const DB = require("./Develop/db/DB.js")
 const app = express()
 const PORT = process.env.PORT || 3000
-const express = require('express')
-const path = require('path')
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,"index.html"))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,"./Develop/public/index.html"))
 })
 
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname,"notes.html"))
+  res.sendFile(path.join(__dirname,"./Develop/public/notes.html"))
 })
 
-app.listen(port, () => console.log(`Note Taker App listening on port:${PORT}!`))
+app.get('/api/notes', async (req, res) => {
+  const currentNotes = await DB.readNotes()
+  console.log(currentNotes);
+  res.json(currentNotes)
+})
+
+app.post('/api/notes', async (req, res) => {
+  const newNote = req.body;
+  const currentNotes = await DB.readNotes()
+})
+
+app.listen(PORT, () => console.log(`Note Taker App listening on port:${PORT}`))
